c02874ea06c74fcdfd58b728e01f831dc6a4683e
Justin Aurand <justin.aurand@swiftcapital.com>
2019-06-01 18:45:40 -0400

    remove unused classes

--- Pizza/NyStyleCheesePizza.java
+++
@@ -1,9 +0,0 @@
-public class NyStyleCheesePizza extends Pizza {
-    public NyStyleCheesePizza() {
-        name = "NY Style Sauce and Cheese Pizza";
-        dough = "Thin Crust Dough";
-        sauce = "Marinara Sauce";
-
-        toppings.add("Grated Reggiano Cheese");
-    }
-}
--- WeatherStation/WeatherData.java
+++ WeatherStation/WeatherData.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Observable;
 
 public class WeatherData extends Observable {
